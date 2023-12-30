deb7635782e087e8db6f393e3dc53e6cba46017c
underground0930 <go.nishiduka.1985@gmail.com>
2023-12-30 19:31:28 +0900

    prettierをインストール

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  plugins: [],
+  semi: false, // セミコロン無し
+  singleQuote: true, // シングルクォート使う
+  printWidth: 100, // 折り返し
+  tabWidth: 2, // スペースの単位
+  trailingComma: 'all', // ケツカンマつける
+  jsxSingleQuote: true, //　JSXでシングルクォートを使用
+}
