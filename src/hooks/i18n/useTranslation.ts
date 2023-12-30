ce13cac2c198fd4ff3f807587421207b0b308f24
underground0930 <go.nishiduka.1985@gmail.com>
2023-12-30 19:32:11 +0900

    翻訳用のhooksを作成

---
+++ src/hooks/i18n/useTranslation.ts
@@ -0,0 +1,46 @@
+import { createInstance } from 'i18next'
+import { Metadata } from 'next'
+import { initReactI18next } from 'react-i18next/initReactI18next'
+
+export const defaultLanguage = 'ja' as const
+export const otherLanguages = ['en'] as const
+export const languages = [defaultLanguage, ...otherLanguages] as const
+export type Languages = (typeof languages)[number]
+
+export type TranslationCommonDef = {
+  [L in Languages]: {
+    translation: {
+      [key: string]: any
+    }
+  }
+}
+export type TranslationDef = {
+  [L in Languages]: {
+    meta: Metadata
+    translation: {
+      [key: string]: any
+    }
+  }
+}
+
+export const useTranslation = ({
+  lang,
+  translation,
+}: {
+  lang: Languages
+  translation: TranslationDef | TranslationCommonDef
+}) => {
+  const i18n = createInstance()
+  void i18n.use(initReactI18next).init({
+    resources: translation,
+    lng: lang,
+    fallbackLng: defaultLanguage,
+    supportedLngs: languages,
+    debug: false,
+  })
+
+  return {
+    t: i18n.getFixedT(lang),
+    i18n,
+  }
+}
